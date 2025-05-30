db6d4d0aff5a77b848b7aa4c115eb2c1657ce4f7
sandwichlover101 <slange26@belmontschools.net>
2025-05-30 10:48:32 -0400

    Add files via upload

---
+++ level9/level9.js
@@ -0,0 +1,9 @@
+function criminal() {
+  let guess= prompt("Type in who is guilty");
+  if (guess=="William Dej") {
+    alert("Yay you guessed correctly");
+  }
+  else {
+    alert("wrong");
+  }
+} 
